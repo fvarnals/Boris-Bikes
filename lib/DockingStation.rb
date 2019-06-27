b525fa17f4a901cc07b2b03ef8dcdefa77930d93
Richie <richieganney@gmail.com>
2019-06-27 15:49:32 +0100

    Refactored using private methods to ensure Single Responsibility

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -15,15 +15,24 @@ class DockingStation
   # end
 
   def release_bike
-    @bikes.empty? ? (raise "Error: no bikes available") : (@bikes.delete_at(0))
+    empty? ? (raise "Error: no bikes available") : (@bikes.delete_at(0))
   end
 
   def dock(bike)
-    @bikes.length >= 20 ? (raise "Error: capacity full") : (@bikes << bike)
+    full? ? (raise "Error: capacity full") : (@bikes << bike)
     #@bike = bike
   end
 
+  private
 
+  def full?
+    @bikes.length >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+  
 end
 
 #binding.pry
