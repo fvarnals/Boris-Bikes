1f7f25cb76ebd18a9a8e20bdc6c21000b217dd1f
jamie palmer <james.palmer@cantab.net>
2019-06-28 15:03:29 +0100

    finished step 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,18 +6,18 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock) }
 
   it "when a bike is docked, we can see that a bike has been docked" do
-    bike1 = Bike.new
+    bike1 = double(:bike)
     expect(subject.dock(bike1)).to eq subject.bikes
   end
-  
+
   it "when there are no bikes docked, should raise an error to release bikes" do
     expect {subject.release_bike}.to raise_error("Error: no bikes available")
   end
-  
+
   it "when the bike station is full, should raise an error to say it's full" do
     station = DockingStation.new
-    subject.capacity.times { station.dock Bike.new }
-    expect { station.dock(Bike.new) }.to raise_error("Error: capacity full")
+    subject.capacity.times { station.dock double(:bike) }
+    expect { station.dock(double(:bike)) }.to raise_error("Error: capacity full")
   end
 
   it "when no capacity is specified, capacity will equal default capacity (20) " do
@@ -27,13 +27,13 @@ describe DockingStation do
   it "when a capacity of '7' is specified, capacity will equal '7' " do
     expect(DockingStation.new(7).capacity).to eq 7
   end
-
+  let(:bike) {double :bike}
   it "it does not let you release a broken bike from the docking station" do
-    bike = Bike.new
-    bike.report
+    #allow(bike).to receive(:broken).and_return(true)
+    bike = double("bike", :broken => true)
     subject.dock(bike)
     expect {subject.release_bike}.to raise_error("Error: cannot release broken bike")
   end
-    
+
 
 end
